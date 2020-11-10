d01aea241d66b4f73311717ed73ad551879a50cf
hanszman <victor.hanszman@hotmail.com>
2020-11-10 18:54:40 -0300

    ajustes

--- api.js
+++ api.js
@@ -10,9 +10,9 @@ api.use(bodyparser.urlencoded({extended: true}));
 api.use(bodyparser.json({limit: '20mb', extended: true}));
 
 router.get("/", (req, res) => res.json({
-    mensagem: 'API Online...'
+    mensagem: 'API Online!'
 }));
 
 api.use("/", router);
+
 api.listen(porta);
-console.log("Run API Express");
