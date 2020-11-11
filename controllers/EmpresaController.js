e02c2c4b020a4ab7753185ccaf13e154e5311710
hanszman <victor.hanszman@hotmail.com>
2020-11-11 20:13:58 -0300

    testando queries, models e conexão com o banco

--- controllers/EmpresaController.js
+++ controllers/EmpresaController.js
@@ -3,6 +3,9 @@ const empresaModel = require('../models/EmpresaModel');
 
 // Funções do Controller
 const empresaRead = async (request, response) => {
+    empresaModel.selectEmpresas(function(erro, retorno) {
+        console.log(retorno);
+    });
     var result = new Object();
     result['tabela'] = 'Empresa';
     response.status(200).json({error: false, data: result});
--- controllers/UsuarioController.js
+++ controllers/UsuarioController.js
@@ -3,6 +3,9 @@ const usuarioModel = require('../models/UsuarioModel');
 
 // Funções do Controller
 const usuarioRead = async (request, response) => {
+    usuarioModel.selectUsuarios(function(erro, retorno) {
+        console.log(retorno);
+    });
     var result = new Object();
     result['tabela'] = 'Usuário';
     response.status(200).json({error: false, data: result});
