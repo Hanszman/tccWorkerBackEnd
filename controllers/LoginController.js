3552799323edd24659b84f9a83b6a621ac428719
hanszman <victor.hanszman@hotmail.com>
2020-12-04 16:53:44 -0300

    ajustes no json response do auth login

--- controllers/LoginController.js
+++ controllers/LoginController.js
@@ -8,19 +8,23 @@ const usuarioModel = require('../models/UsuarioModel');
 const loginAuth = async (request, response) => {
     var dados = request.body;
     usuarioModel.authSenhaLogin(dados.dsc_login, function(erro, retorno) {
+        var result = new Object();
         var jsonRetorno = JSON.parse(JSON.stringify(retorno));
         if (jsonRetorno.length == 1) {
-            if (bcrypt.compareSync(dados.dsc_senha, jsonRetorno[0]['dsc_senha']))
-                console.log('OK');
-            else
-                console.log('Senha incorreta!');
+            if (bcrypt.compareSync(dados.dsc_senha, jsonRetorno[0]['dsc_senha'])) {
+                result['sucesso'] = true;
+                result['mensagem'] = 'Login realizado com sucesso!';
+            }
+            else {
+                result['sucesso'] = false;
+                result['mensagem'] = 'Senha incorreta!';
+            }   
         }
         else {
-            console.log('Usuário não existe!');
+            result['sucesso'] = false;
+            result['mensagem'] = 'Usuário não existe!';
         }
-        var resposta = new Object();
-        resposta.dados = retorno;
-        response.json(resposta);
+        response.status(200).json({error: false, data: result});
     });
 };
 
