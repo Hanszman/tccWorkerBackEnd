f4e5d81ed6c5eabc0b8f6ba1a2596dd851921296
hanszman <victor.hanszman@hotmail.com>
2020-12-04 16:41:58 -0300

    ajustes no auth do login

--- controllers/LoginController.js
+++ controllers/LoginController.js
@@ -7,30 +7,21 @@ const usuarioModel = require('../models/UsuarioModel');
 // Funções do Controller
 const loginAuth = async (request, response) => {
     var dados = request.body;
-    // console.log(dados.dsc_senha);
-    const salt = bcrypt.genSaltSync(10);
-    const hash = bcrypt.hashSync('teste', salt);
-    // console.log(hash);
-    // console.log(bcrypt.compareSync('teste', '$2a$10$UZXZSGxL/JQncOVFT1bNtOfd0nPHFFbPTYtyuVkPLEHJYBveD6d/G')); // Irá retornar true.
-    // console.log(bcrypt.compareSync('asdasdasd', '$2a$10$UZXZSGxL/JQncOVFT1bNtOfd0nPHFFbPTYtyuVkPLEHJYBveD6d/G')); // Irá retornar false.
     usuarioModel.authSenhaLogin(dados.dsc_login, function(erro, retorno) {
-        console.log(retorno);
+        var jsonRetorno = JSON.parse(JSON.stringify(retorno));
+        if (jsonRetorno.length == 1) {
+            if (bcrypt.compareSync(dados.dsc_senha, jsonRetorno[0]['dsc_senha']))
+                console.log('OK');
+            else 
+                console.log('Senha incorreta!');
+        }
+        else {
+            console.log('Usuário não existe!');
+        }
         var resposta = new Object();
         resposta.dados = retorno;
         response.json(resposta);
     });
-    // Instruções:
-    // const bcrypt = require('bcryptjs');
-    // const password = '123';
-    // const salt = bcrypt.genSaltSync(10);
-    // const hash = bcrypt.hashSync(password, salt); // Guarde o `hash` na sua base de dados...
-    // const db_password = db.password; // Imagine que veio da base de dados.
-    // bcrypt.compareSync('123', db_password); // Irá retornar true.
-    // bcrypt.compareSync('456', db_password); // Irá retornar false.
-    
-    // var result = new Object();
-    // result['mensagem'] = 'login';
-    // response.status(200).json({error: false, data: result});
 };
 
 // Exportando Funções
