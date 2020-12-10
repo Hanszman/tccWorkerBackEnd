19d90974f9bda0e89b032cb4f5313d5d8a6509c8
hanszman <victor.hanszman@hotmail.com>
2020-12-10 20:30:14 -0300

    ajustes na função de empresa

--- controllers/EmpresaController.js
+++ controllers/EmpresaController.js
@@ -4,11 +4,13 @@ const empresaModel = require('../models/EmpresaModel');
 // Funções do Controller
 const empresaRead = async (request, response) => {
     empresaModel.selectEmpresas(function(erro, retorno) {
-        console.log(retorno);
+        var result = new Object();
+        var jsonRetorno = JSON.parse(JSON.stringify(retorno));
+        for (let i = 0; i < jsonRetorno.length; i++){
+            console.log(jsonRetorno[i]);
+        }
+        response.status(200).json({error: false, data: result});
     });
-    var result = new Object();
-    result['tabela'] = 'Empresa';
-    response.status(200).json({error: false, data: result});
 };
 
 // Exportando Funções
