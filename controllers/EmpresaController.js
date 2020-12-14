4b2f3276bdf46e265c36e2c97d79d178ba80643a
hanszman <victor.hanszman@hotmail.com>
2020-12-14 18:07:22 -0300

    ajustes na função de exibição de empresas

--- controllers/EmpresaController.js
+++ controllers/EmpresaController.js
@@ -4,11 +4,7 @@ const empresaModel = require('../models/EmpresaModel');
 // Funções do Controller
 const empresaRead = async (request, response) => {
     empresaModel.selectEmpresas(function(erro, retorno) {
-        var result = new Object();
-        var jsonRetorno = JSON.parse(JSON.stringify(retorno));
-        for (let i = 0; i < jsonRetorno.length; i++){
-            console.log(jsonRetorno[i]);
-        }
+        var result = JSON.parse(JSON.stringify(retorno));
         response.status(200).json({error: false, data: result});
     });
 };
