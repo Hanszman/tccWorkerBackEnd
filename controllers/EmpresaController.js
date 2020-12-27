ca0b022c6b652a4507f0f2894510c7bbafee0074
hanszman <victor.hanszman@hotmail.com>
2020-12-27 02:40:19 -0300

    ajustes

--- controllers/EmpresaController.js
+++ controllers/EmpresaController.js
@@ -20,9 +20,7 @@ const empresaRead = async (request, response) => {
                 result[i]['ind_controle_acesso'] = indControleAcesso(result[i]['ind_controle_acesso']);
                 result[i]['ind_contratacao'] = indContratacao(result[i]['ind_contratacao']);
                 result[i]['ind_status'] = indStatus(result[i]['ind_status']);
-                result[i]['qtd_usuario'] = '';
             }
-            console.log(result);
             response.status(200).json({error: false, data: result});
         });
     }  
