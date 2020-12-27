f3a15c133e0225c3ace301b240aafe221882ae2b
hanszman <victor.hanszman@hotmail.com>
2020-12-27 02:21:48 -0300

    ajustes

--- models/EmpresaModel.js
+++ models/EmpresaModel.js
@@ -3,7 +3,7 @@ const db = require('../database/conexao');
 
 // Exportando a Classe do Model
 module.exports = class EmpresaModel {
-    static selectEmpresas(id_usuario, dsc_nome, callback) {
+    static selectEmpresas (id_usuario, dsc_nome, callback) {
         return db.query("SELECT * FROM empresa e JOIN usuario_empresa ue ON ue.id_empresa = e.id_empresa WHERE ue.id_usuario = ? AND e.dsc_nome like ?",
         [id_usuario, dsc_nome],
         callback);
