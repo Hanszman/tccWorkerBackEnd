2c9c9e329ad1213cb1dfb9e66191ec8db1a2a72b
hanszman <victor.hanszman@hotmail.com>
2021-01-07 18:28:13 -0300

    funções de paginação

--- controllers/GeralController.js
+++ controllers/GeralController.js
@@ -2,11 +2,38 @@
 var moment = require('moment');
 
 // Funções do Controller
+const configuraPaginacao = (pagina, paginacao, padrao = 999999999) => {
+    pagina = pagina ? parseInt(pagina) : 1;
+    paginacao = paginacao ? parseInt(paginacao) : padrao;
+    return { pagina, paginacao };
+};
+
+const aplicaPaginacao = (configuracao, colecao) => {
+    const total = colecao.length;
+    const porPagina = configuracao.paginacao;
+    const paginaAtual = configuracao.pagina;
+    var ultimaPagina = parseInt(total/porPagina);
+    ultimaPagina = (total % porPagina > 0) ? (ultimaPagina + 1) : ultimaPagina;
+    ultimaPagina = total > porPagina ? ultimaPagina : 1;
+    const comeco = (paginaAtual - 1) * porPagina;
+    conjuntoDados = colecao.slice(comeco, comeco + porPagina);
+    const result = {
+        'total': total,
+        'por_pagina': porPagina,
+        'pagina_atual': paginaAtual,
+        'ultima_pagina': ultimaPagina,
+        'de': (conjuntoDados.length !== 0) ? comeco + 1 : null,
+        'ate': (conjuntoDados.length !== 0) ? comeco + conjuntoDados.length : null,
+        'dados': conjuntoDados
+    }
+    return result;
+};
+
 const formatoData = (data) => {
     var dataFormato = moment.utc(data);
     dataFormato = moment(dataFormato).format('DD/MM/YYYY').toString();
     return dataFormato;
-}
+};
 
 const indControleAcesso = (indicador) => {
     var result;
@@ -24,7 +51,7 @@ const indControleAcesso = (indicador) => {
             result = 'Comum';
     }
     return result;
-}
+};
 
 const indContratacao = (indicador) => {
     var result;
@@ -42,7 +69,7 @@ const indContratacao = (indicador) => {
             result = 'Carteira Assinada';
     }
     return result;
-}
+};
 
 const indStatus = (indicador) => {
     var result;
@@ -57,10 +84,12 @@ const indStatus = (indicador) => {
             result = 'Ativo';
     }
     return result;
-}
+};
 
 // Exportando Funções
 module.exports = {
+    configuraPaginacao,
+    aplicaPaginacao,
     formatoData,
     indControleAcesso,
     indContratacao,
