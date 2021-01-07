dd205ee8c8040e625bcda3869989ac9911e492bd
hanszman <victor.hanszman@hotmail.com>
2021-01-07 20:56:31 -0300

    ajustes nos filtros de usuario

--- models/UsuarioModel.js
+++ models/UsuarioModel.js
@@ -15,9 +15,17 @@ const selectUsuario = async (id_usuario, parametros, dsc_login) => {
             query.select('ue.*');
             query.join({ ue: "usuario_empresa" }, "ue.id_usuario", "=", "u.id_usuario");
             query.andWhere('ue.id_empresa', '=', parametros.id_empresa);
+            if (parametros.dsc_cargo)
+                query.andWhere('ue.dsc_cargo', 'like', '%' + parametros.dsc_cargo + '%');
+            if (parametros.ind_controle_acesso)
+                query.andWhere('ue.ind_controle_acesso', 'like', '%' + parametros.ind_controle_acesso + '%');
+            if (parametros.ind_status)
+                query.andWhere('ue.ind_status', 'like', '%' + parametros.ind_status + '%');
         }
         if (parametros.dsc_nome)
             query.andWhere('u.dsc_nome', 'like', '%' + parametros.dsc_nome + '%');
+        if (parametros.dsc_email)
+            query.andWhere('u.dsc_email', 'like', '%' + parametros.dsc_email + '%');
         if (parametros.dsc_nome_completo)
             query.andWhere(knex.raw("concat(u.dsc_nome, ' ', u.dsc_sobrenome)"), 'like', '%' + parametros.dsc_nome_completo + '%');
         if (parametros.dsc_login)
