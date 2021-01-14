f51124e427a3016e07008a193c32f39330d76166
hanszman <victor.hanszman@hotmail.com>
2021-01-14 17:28:29 -0300

    ajustes no .env

--- database/conexao.js
+++ database/conexao.js
@@ -1,11 +1,11 @@
 // Conexão com o Banco de Dados
 const Knex = require('knex')({
-    client: process.env.DB_CLIENT,
+    client: process.env.DB_CLIENT || 'mysql',
     connection: {
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_SCHEMA,
+        host: process.env.DB_HOST || 'us-cdbr-east-02.cleardb.com',
+        user: process.env.DB_USER || 'bff06fae24e971',
+        password: process.env.DB_PASSWORD || 'e14a8362',
+        database: process.env.DB_SCHEMA || 'heroku_9439eb1c84438d2',
         timezone: 'UTC'
     }
 });
