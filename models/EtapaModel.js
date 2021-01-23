4c03e9c25f51f1e6cf93e431a20c9323851ea80f
hanszman <victor.hanszman@hotmail.com>
2021-01-23 00:21:00 -0300

    funções do model de etapas

--- models/EtapaModel.js
+++ models/EtapaModel.js
@@ -3,19 +3,61 @@ const knex = require('../database/conexao');
 
 // Funções do Model
 const selectEtapa = async (id_etapa, parametros) => {
-    
+    let query = knex('etapa')
+    .where(1, '=', 1);
+    if (id_etapa)
+        query.andWhere('id_etapa', '=', id_etapa);
+    if (parametros) {
+        if (parametros.id_empresa)
+            query.andWhere('id_empresa', '=', parametros.id_empresa);
+        if (parametros.dsc_etapa)
+            query.andWhere('dsc_etapa', 'like', '%' + parametros.dsc_etapa + '%');
+        if (parametros.ind_sequencia)
+            query.andWhere('ind_sequencia', 'like', '%' + parametros.ind_sequencia + '%');
+        if(parametros.ordenarPor){
+            if(parametros.direcao)
+                query.orderBy(parametros.ordenarPor, parametros.direcao);
+            else
+                query.orderBy(parametros.ordenarPor, "asc");
+        }
+    }
+    let result = await query;
+    return result;
 };
 
 const insertEtapa = async (dados) => {
-    
+    let query = knex('etapa')
+    .insert({
+        dsc_etapa: dados.dsc_etapa,
+        ind_sequencia: dados.ind_sequencia,
+        id_empresa: dados.id_empresa_logada
+    }).returning('id_etapa');
+    let result = await query;
+    return result;
 };
 
 const updateEtapa = async (id, dados) => {
-    
+    let query = knex('etapa')
+    .update({
+        dsc_etapa: dados.dsc_etapa,
+        ind_sequencia: dados.ind_sequencia,
+        id_empresa: dados.id_empresa_logada
+    }).where('id_etapa', '=', id)
+    let result = await query;
+    return result;
 };
 
 const deleteEtapa = async (id) => {
-    
+    try {
+        let query = knex('etapa')
+        .delete()
+        .where('id_etapa', '=', id);
+        let result = await query;
+        return result;
+    }
+    catch (erro) {
+        return 'Erro: ' + erro;
+    }
 };
 
 // Exportando Funções
