e0ae8388f4c71bc6f9223f1ffbb0c1c125ab291b
hanszman <victor.hanszman@hotmail.com>
2021-02-13 19:03:31 -0300

    ajustes

--- controllers/ChartController.js
+++ controllers/ChartController.js
@@ -5,9 +5,12 @@ const chartModel = require('../models/ChartModel');
 
 // GRÁFICOS
 // 1-) Quantidade de Atividades por Etapa (Rosca)
-// 2-) Quantidade de Atividades por Setor e por Etapa (Barra Stacked)
-// 3-) Quantidade de Atividades por Prioridade e por Etapa (Barra Stacked)
-// 4-) Quantidade de Atividades por Funcionários e por Etapa (Barra Horizontal Stacked)
+// 2-) Quantidade de Atividades por Prioridade e por Etapa (Barra Stacked)
+// 3-) Quantidade de Atividades por Setor e por Etapa (Barra Stacked)
+// 4-) Quantidade de Atividades por Cliente e por Etapa (Barra Stacked)
+// 5-) Quantidade de Atividades por Fornecedor e por Etapa (Barra Stacked)
+// 6-) Quantidade de Atividades por Funcionários e por Etapa (Barra Horizontal Stacked)
+// 7-) Quantidade de Atividades por Projeto e por Etapa (Barra Stacked)
 
 // Exportando Funções
 module.exports = {
