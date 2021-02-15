e527080d385b09e45797f0c112dca70f648f34a7
hanszman <victor.hanszman@hotmail.com>
2021-02-15 14:42:10 -0300

    ajustes

--- controllers/ChartController.js
+++ controllers/ChartController.js
@@ -4,12 +4,12 @@ const chartModel = require('../models/ChartModel');
 // Funções do Controller
 
 // GRÁFICOS
-// 1-) Quantidade de Atividades por Etapa (Pizza)
-// 2-) Quantidade de Atividades por Prioridade e por Etapa (Barra Stacked)
+// 1-) Quantidade de Atividades por Etapa (Pizza) ***
+// 2-) Quantidade de Atividades por Prioridade e por Etapa (Barra Stacked) ***
 // 3-) Quantidade de Atividades por Cliente e por Etapa (Barra Stacked)
 // 4-) Quantidade de Atividades por Fornecedor e por Etapa (Barra Stacked)
-// 5-) Quantidade de Atividades por Funcionário e por Etapa (Barra Horizontal Stacked)
-// 6-) Quantidade de Atividades por Setor e por Etapa (Barra Stacked)
+// 5-) Quantidade de Atividades por Funcionário e por Etapa (Barra Horizontal Stacked) ***
+// 6-) Quantidade de Atividades por Setor e por Etapa (Barra Stacked) ***
 // 7-) Quantidade de Atividades por Projeto e por Etapa (Barra Stacked)
 // 8-) Quantidade de Projetos por Cliente (Pizza)
 // 9-) Quantidade de Projetos por Fornecedor (Pizza)
