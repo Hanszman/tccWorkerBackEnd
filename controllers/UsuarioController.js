994a65ca37b3e30a3fe8d795f89479867b2664d5
hanszman <victor.hanszman@hotmail.com>
2021-03-13 15:04:22 -0300

    ajustes na edição de usuário com fb no backend

--- controllers/UsuarioController.js
+++ controllers/UsuarioController.js
@@ -91,37 +91,41 @@ const usuarioUpdate = async (request, response) => {
     var dados = request.body;
     var querySelect = await usuarioModel.selectUsuario(id_usuario);
     var old_dsc_senha = querySelect[0]['dsc_senha'];
-    if (dados.dsc_confirm_senha == dados.dsc_senha) {
-        if (dados.dsc_senha !== old_dsc_senha) {
-            const password = dados.dsc_senha;
-            const salt = bcrypt.genSaltSync(10);
-            const hash = bcrypt.hashSync(password, salt);
-            dados.dsc_senha = hash;
-        }   
-        if (dados.old_arq_foto)
-            dados.caminho_arq_foto = dados.old_arq_foto;
-        else if (dados.dados_arq_foto && dados.arq_foto) {
-            let bitmap = new Buffer.from(dados.dados_arq_foto.imagem_base64, 'base64');
-            let dataAtual = new Date().toLocaleString().replace(/\//g, '').replace(/:/g, '').replace(/-/g, '').replace(/ /g, '');
-            let nomeImagemCaminho = './files/img/' + dataAtual + dados.dados_arq_foto.nome_arquivo;
-            fs.writeFileSync(nomeImagemCaminho, bitmap);
-            dados.caminho_arq_foto = nomeImagemCaminho;
-        }
-        else
-            dados.caminho_arq_foto = null;
-        var queryUpdate = await usuarioModel.updateUsuario(id_usuario, dados);
-        if (queryUpdate > 0) {
-            result['sucesso'] = true;
-            result['mensagem'] = 'Usuário editado com sucesso!';
+    if (querySelect[0]['ind_login_fb'] == 'N') {
+        if (dados.dsc_confirm_senha == dados.dsc_senha) {
+            if (dados.dsc_senha !== old_dsc_senha) {
+                const password = dados.dsc_senha;
+                const salt = bcrypt.genSaltSync(10);
+                const hash = bcrypt.hashSync(password, salt);
+                dados.dsc_senha = hash;
+            }
         }
         else {
             result['sucesso'] = false;
-            result['mensagem'] = 'Erro ao editar usuário!';
+            result['mensagem'] = 'Senhas não conferem!';
+            response.status(200).json({error: false, data: result});
+            return;
         }
+    }
+    if (dados.old_arq_foto)
+        dados.caminho_arq_foto = dados.old_arq_foto;
+    else if (dados.dados_arq_foto && dados.arq_foto) {
+        let bitmap = new Buffer.from(dados.dados_arq_foto.imagem_base64, 'base64');
+        let dataAtual = new Date().toLocaleString().replace(/\//g, '').replace(/:/g, '').replace(/-/g, '').replace(/ /g, '');
+        let nomeImagemCaminho = './files/img/' + dataAtual + dados.dados_arq_foto.nome_arquivo;
+        fs.writeFileSync(nomeImagemCaminho, bitmap);
+        dados.caminho_arq_foto = nomeImagemCaminho;
+    }
+    else
+        dados.caminho_arq_foto = null;
+    var queryUpdate = await usuarioModel.updateUsuario(id_usuario, dados);
+    if (queryUpdate > 0) {
+        result['sucesso'] = true;
+        result['mensagem'] = 'Usuário editado com sucesso!';
     }
     else {
         result['sucesso'] = false;
-        result['mensagem'] = 'Senhas não conferem!';
+        result['mensagem'] = 'Erro ao editar usuário!';
     }
     response.status(200).json({error: false, data: result});
 };
