93e4804b38380825a5ada362f20fe4d475b95bb4
hanszman <victor.hanszman@hotmail.com>
2021-03-13 15:44:15 -0300

    ajustes

--- controllers/LoginController.js
+++ controllers/LoginController.js
@@ -50,7 +50,7 @@ const loginAuthFB = async (request, response) => {
         dados['dsc_login'] = userInfo.data.email;
         dados['dsc_senha'] = userInfo.data.id;
         dados['ind_login_fb'] = 'S';
-        // dados['arq_foto'] = userInfo.data.picture.data.url;
+        // dados['arq_foto'] = userInfo.data.picture.data.url; // Seria necessário redirecionar o download para a pasta de arquivos
         var queryInsert = await usuarioModel.insertUsuario(dados);
         if (queryInsert.length <= 0) {
             result['sucesso'] = false;
