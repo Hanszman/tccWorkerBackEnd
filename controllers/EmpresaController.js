d91a5bbd6044d83628643e09410a9a3383ed9b44
hanszman <victor.hanszman@hotmail.com>
2021-03-29 11:25:36 -0300

    ajustes

--- controllers/EmpresaController.js
+++ controllers/EmpresaController.js
@@ -22,8 +22,10 @@ const empresaRead = async (request, response) => {
             result[i]['dsc_cnpj'] = formatoCNPJ(result[i]['dsc_cnpj']);
             result[i]['dat_fundacao'] = formatoData(result[i]['dat_fundacao']);
             result[i]['dat_contratacao'] = formatoData(result[i]['dat_contratacao']);
+            result[i]['ind_controle_acesso_bruto'] = result[i]['ind_controle_acesso'];
             result[i]['ind_controle_acesso'] = indControleAcesso(result[i]['ind_controle_acesso']);
             result[i]['ind_contratacao'] = indContratacao(result[i]['ind_contratacao']);
+            result[i]['ind_status_bruto'] = result[i]['ind_status'];
             result[i]['ind_status'] = indStatus(result[i]['ind_status']);
         }
         response.status(200).json({error: false, data: result});
